37c1f4a1c6568b7f6a77e2b93c135295d430bfdd
leandronascimento <lnascimentodesouza@gmail.com>
2022-03-10 20:33:43 -0300

    usando dois beans com laço for e list

--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -5,33 +5,19 @@ import com.algaworks.algafood.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AtivacaoClienteService {
 
-    @Autowired
-    private Notificador notificador;
-
-//    @Autowired // indicando em qual construtor deve ser injentado a dependencia Autowired
-//    public AtivacaoClienteService(Notificador notificador) {
-//        this.notificador = notificador;
-//    }
-//
-//    public AtivacaoClienteService(String qualquer) {
-//
-//    }
-
-
-    public Notificador getNotificador() {
-        return notificador;
-    }
-
-//    @Autowired
-//    public void setNotificador(Notificador notificador) {
-//        this.notificador = notificador;
-//    }
+    @Autowired(required = false)
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente) {
         cliente.isAtivo();
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+
+        for (Notificador notificador : notificadores) {
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo");
+        }
     }
 }
