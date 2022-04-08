ef7520302b4667f3daf7df243a424116423abbe2
sntechbio <lnascimentodesouza@gmail.com>
2022-04-08 17:06:02 -0300

    inclusão de testes de integração

---
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIT.java
@@ -0,0 +1,33 @@
+package com.algaworks.algafood;
+
+import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradoException;
+import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class CadastroCozinhaIT {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void deve_retornar_status_200_quando_consultar_cozinha() {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.given().basePath("/cozinhas").port(8080).accept(ContentType.JSON).when().get().then().statusCode(200);
+    }
+
+}
